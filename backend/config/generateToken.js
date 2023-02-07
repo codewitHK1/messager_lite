05824dfc925019c389f2dc43d6cf20d9b1df7a8a
hitesh <kumarhk1002@gmail.com>
2023-02-07 11:34:20 -0800

    2 commit

---
+++ backend/config/generateToken.js
@@ -0,0 +1,11 @@
+/** @format */
+
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "20d",
+  });
+};
+
+module.exports = generateToken;
---
+++ backend/models/userModel.js
@@ -0,0 +1,30 @@
+/** @format */
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    picture: { type: String, default: "" },
+  },
+  {
+    timestamps: true,
+  }
+);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.method.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
